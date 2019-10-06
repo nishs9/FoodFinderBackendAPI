481289f1dbd8c6233b33d8d9c185e0ffbe625242
Nishant Sethunath <snishk@gmail.com>
2019-10-06 17:22:21 -0400

    Small updates to the endpoints

--- index.js
+++ index.js
@@ -28,18 +28,18 @@ app.post("/users", (request, response) => {
   });
 });
 
-//GET endpoint that retrieves all users and their information (exclusing password)
-app.get("/users", (request, response) => {
-    collection.find({}).toArray((error, result) => {
-      if(error) {
-        return response.status.send(error);
-      }
-      response.send(result);
+//GET endpoint that retrieves a specfiic user given their unique user ID
+app.get("/users/:id", (request, response) => {
+    collection.findOne({"_id" : new ObjectId(request.params.id) }, (error, result) => {
+        if(error) {
+          return response.status(500).send(error);
+        }
+        response.send(result);
     });
 });
 
 app.listen(port, () => {
-    MongoClient.connect(CONNECTION_URL, { userNewUrlParser: true}, (error, client) => {
+    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true}, (error, client) => {
         if(error) {
           throw error;
         }
